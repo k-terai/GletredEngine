b16b4bd74d2737d56237c4bf0f0b1a920d7e1b82
k-terai <23315372+k-terai@users.noreply.github.com>
2023-05-20 04:38:24 +0900

    Add CustomTheme

---
+++ sources/Editor/GletredWpfEditor/Main/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) k-terai and Contributors
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace GletredWpfEditor.Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
