e90ae894cc0ba2204880baa2ff53dd2c43cc8972
k-terai <23315372+k-terai@users.noreply.github.com>
2023-05-20 05:19:31 +0900

    Add ControlModule

---
+++ sources/Share/GletredEdShare/ControlModule/ControlViewModel.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) k-terai and Contributors
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using GletredEdShare.CoreModule;
+
+namespace GletredEdShare.ControlModule
+{
+    public class ControlViewModel : ViewModelBase
+    {
+        private string _title = null!;
+        private bool _enableToolBar;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool EnableToolBar
+        {
+            get => _enableToolBar;
+            set
+            {
+                _enableToolBar = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ControlViewModel()
+        {
+            Title = string.Empty;
+            EnableToolBar = true;
+        }
+    }
+}
---
+++ sources/Share/GletredEdShare/ControlModule/IControl.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) k-terai and Contributors
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+
+#if WPF
+using System.Windows.Controls;
+#endif
+
+namespace GletredEdShare.ControlModule
+{
+    public interface IControl
+    {
+#if WPF
+        UserControl Control { get; }
+#endif
+    }
+}
