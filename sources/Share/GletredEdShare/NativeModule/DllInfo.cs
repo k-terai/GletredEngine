f40d5bffcfd492c130fea2f8ebe657d01264eaac
k-terai <23315372+k-terai@users.noreply.github.com>
2023-05-20 15:52:44 +0900

    Add NativeModule

---
+++ sources/Share/GletredEdShare/NativeModule/DllInfo.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) k-terai and Contributors
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+
+namespace GletredEdShare.NativeModule
+{
+    public struct DllInfo
+    {
+        public Guid Id { get; set; }
+
+        public DllInfo()
+        {
+            Id = Guid.Empty;
+        }
+    }
+}
---
+++ sources/Share/GletredEdShare/NativeModule/NativeLibraryManager.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) k-terai and Contributors
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace GletredEdShare.NativeModule
+{
+    public static class NativeLibraryManager
+    {
+        private static readonly Dictionary<Guid, IntPtr> LoadLib = new();
+
+        public static bool LoadNativeLibrary(in DllContext context, out DllInfo info)
+        {
+            info = new DllInfo();
+
+            if (File.Exists(context.FilePath) == false)
+            {
+                return false;
+            }
+
+            info.Id = Guid.NewGuid();
+            var ptr = NativeLibrary.Load(context.FilePath);
+            if (ptr == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            LoadLib[info.Id] = ptr;
+            return true;
+        }
+
+        public static void FreeNativeLibrary(in Guid id)
+        {
+            if (!LoadLib.ContainsKey(id))
+            {
+                return;
+            }
+
+            NativeLibrary.Free(LoadLib[id]);
+            LoadLib.Remove(id);
+        }
+
+        public static T GetNativeDelegate<T>(in Guid id)
+            where T : Delegate
+        {
+            if (LoadLib.ContainsKey(id) == false)
+            {
+                return null!;
+            }
+
+            var export = NativeLibrary.GetExport(LoadLib[id], typeof(T).Name);
+            return (T)Marshal.GetDelegateForFunctionPointer(export, typeof(T));
+        }
+    }
+}
